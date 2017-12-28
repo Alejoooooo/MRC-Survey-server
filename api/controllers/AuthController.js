cdefd5b30ceefae99b56fafa6f7e4b0588a07586
aleio <raponialessio@hotmail.com>
2017-12-28 06:22:57 +0100

    added telegram for access notification

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -9,34 +9,35 @@ var auth = require('../services/auth');
 var access = require('../services/access');
 
 
+
+
 module.exports = {
-  login: function (req, res) {
-    auth.login(req, res);
-    access.log(req, res);
-  },
-  validate_token: function (req, res) {
-    auth.isvalidtoken(req, res);
-  },
-  logout: function(req, res){
-    req.logout();
-    res.send(200);
-  },
+    login: function (req, res) {
+        auth.login(req, res);
+        access.log(req, res);
+    },
+    validate_token: function (req, res) {
+        auth.isvalidtoken(req, res);
+    },
+    logout: function (req, res) {
+        req.logout();
+        res.send(200);
+    },
 
 
-  hashPassword:function (req, res){
-    var bcrypt = require('bcrypt');
-    const saltRounds = 10;
-    var password = req.body.password;
+    hashPassword: function (req, res) {
+        var bcrypt = require('bcrypt');
+        const saltRounds = 10;
+        var password = req.body.password;
 
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(password, salt, function(err, hash) {
-        return res.json(hash);
-    });
-});
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+                return res.json(hash);
+            });
+        });
 
-    
-}
 
-  
+    }
+
+
 };
-
--- api/services/auth.js
+++ api/services/auth.js
@@ -1,6 +1,10 @@
 var passport = require("passport");
 var jwt = require('jsonwebtoken');
 var access = require('../services/access');
+var telegram = require('telegram-bot-api');
+var api = new telegram({
+    token: '487163058:AAGdE3-02TRtFxPIjO15Bc05CleyKpoqsJk'
+});
 
 module.exports = {
     login: function (req, res) {
@@ -21,13 +25,20 @@ module.exports = {
                 } else {
                     //IN QUESTO PUNTO DOVREBBE FARE LOG ACCESSO
 
-                    var token = jwt.sign(user[0], sails.config.secret, {expiresIn: 6000000 * 24});
+                    var token = jwt.sign(user[0], sails.config.secret, {
+                        expiresIn: 6000000 * 24
+                    });
                     // Set persistent cookie
                     req.session.cookie.token = token;
                     res.send({
                         success: true,
                         user: user[0],
                         token: token
+                    });
+
+                    api.sendMessage({
+                        chat_id: 69886441,
+                        text: user[0]["username"] + " ha effettuato l'accesso al sistema"
                     });
                     //access.log(req, res);
                 }
@@ -37,13 +48,20 @@ module.exports = {
     isvalidtoken: function (req, res) {
         if (req.headers.authorization) {
             jwt.verify(req.headers.authorization.replace('Bearer ', ''), sails.config.secret, function (err, decoded) {
-                if (err) return res.send({success: false});
+                if (err) return res.send({
+                    success: false
+                });
                 if (decoded) {
-                    return res.send({success: true, user: decoded[0]});
+                    return res.send({
+                        success: true,
+                        user: decoded[0]
+                    });
                 }
             });
         } else {
-            return res.send({success: false});
+            return res.send({
+                success: false
+            });
         }
     }
 };
